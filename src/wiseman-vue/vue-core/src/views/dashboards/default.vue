433076339cd877518b2ed52e850922fc2a31258d
rizkiraden <rizkiraden616@gmail.com>
2024-10-23 16:51:12 +0700

    Update atuh firebase

--- src/wiseman-vue/vue-core/src/views/dashboards/default.vue
+++ src/wiseman-vue/vue-core/src/views/dashboards/default.vue
@@ -438,15 +438,6 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCc_sK7DV60666AlMbovDiZJp1YAVNwJgU",
-  authDomain: "wiseman-131e0.firebaseapp.com",
-  projectId: "wiseman-131e0",
-  storageBucket: "wiseman-131e0.appspot.com",
-  messagingSenderId: "695127333701",
-  appId: "1:695127333701:web:f3b5b6facf9f81908dfc2c",
-  measurementId: "G-NVGFK72G38"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
