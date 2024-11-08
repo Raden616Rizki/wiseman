c2cf622207bcb8b248e7d025f465f3e14a8ad2cd
rizkiraden <rizkiraden616@gmail.com>
2024-11-08 17:36:33 +0700

    Menambahakan middleware auth.api untuk seluruh rest api

--- src/wiseman-laravel/laravel-core/routes/api.php
+++ src/wiseman-laravel/laravel-core/routes/api.php
@@ -27,76 +27,76 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->group(function () {
-	Route::get('/user_votings', [UserVotingController::class, 'index']);
-	Route::get('/user_votings/{id}', [UserVotingController::class, 'show']);
-	Route::post('/user_votings', [UserVotingController::class, 'store']);
-	Route::put('/user_votings/{id}', [UserVotingController::class, 'update']);
-	Route::delete('/user_votings/{id}', [UserVotingController::class, 'destroy']);
+	Route::get('/user_votings', [UserVotingController::class, 'index'])->middleware(['auth.api']);
+	Route::get('/user_votings/{id}', [UserVotingController::class, 'show'])->middleware(['auth.api']);
+	Route::post('/user_votings', [UserVotingController::class, 'store'])->middleware(['auth.api']);
+	Route::put('/user_votings/{id}', [UserVotingController::class, 'update'])->middleware(['auth.api']);
+	Route::delete('/user_votings/{id}', [UserVotingController::class, 'destroy'])->middleware(['auth.api']);
 
-	Route::get('/enrollments', [EnrollmentController::class, 'index']);
-	Route::get('/enrollments/{id}', [EnrollmentController::class, 'show']);
-	Route::post('/enrollments', [EnrollmentController::class, 'store']);
-	Route::put('/enrollments/{id}', [EnrollmentController::class, 'update']);
-	Route::delete('/enrollments/{id}', [EnrollmentController::class, 'destroy']);
+	Route::get('/enrollments', [EnrollmentController::class, 'index'])->middleware(['auth.api']);
+	Route::get('/enrollments/{id}', [EnrollmentController::class, 'show'])->middleware(['auth.api']);
+	Route::post('/enrollments', [EnrollmentController::class, 'store'])->middleware(['auth.api']);
+	Route::put('/enrollments/{id}', [EnrollmentController::class, 'update'])->middleware(['auth.api']);
+	Route::delete('/enrollments/{id}', [EnrollmentController::class, 'destroy'])->middleware(['auth.api']);
 
-	Route::get('/memos', [MemoController::class, 'index']);
-	Route::get('/memos/{id}', [MemoController::class, 'show']);
-	Route::post('/memos', [MemoController::class, 'store']);
-	Route::put('/memos/{id}', [MemoController::class, 'update']);
-	Route::delete('/memos/{id}', [MemoController::class, 'destroy']);
+	Route::get('/memos', [MemoController::class, 'index'])->middleware(['auth.api']);
+	Route::get('/memos/{id}', [MemoController::class, 'show'])->middleware(['auth.api']);
+	Route::post('/memos', [MemoController::class, 'store'])->middleware(['auth.api']);
+	Route::put('/memos/{id}', [MemoController::class, 'update'])->middleware(['auth.api']);
+	Route::delete('/memos/{id}', [MemoController::class, 'destroy'])->middleware(['auth.api']);
 
-	Route::get('/archives', [ArchiveController::class, 'index']);
-	Route::get('/archives/{id}', [ArchiveController::class, 'show']);
-	Route::post('/archives', [ArchiveController::class, 'store']);
-	Route::post('/archives/{id}', [ArchiveController::class, 'copy']);
-	Route::put('/archives/{id}', [ArchiveController::class, 'update']);
-	Route::delete('/archives/{id}', [ArchiveController::class, 'destroy']);
+	Route::get('/archives', [ArchiveController::class, 'index'])->middleware(['auth.api']);
+	Route::get('/archives/{id}', [ArchiveController::class, 'show'])->middleware(['auth.api']);
+	Route::post('/archives', [ArchiveController::class, 'store'])->middleware(['auth.api']);
+	Route::post('/archives/{id}', [ArchiveController::class, 'copy'])->middleware(['auth.api']);
+	Route::put('/archives/{id}', [ArchiveController::class, 'update'])->middleware(['auth.api']);
+	Route::delete('/archives/{id}', [ArchiveController::class, 'destroy'])->middleware(['auth.api']);
 
-	Route::get('/votings', [VotingController::class, 'index']);
-	Route::get('/votings/{id}', [VotingController::class, 'show']);
-	Route::post('/votings', [VotingController::class, 'store']);
-	Route::put('/votings/{id}', [VotingController::class, 'update']);
-	Route::put('/votings/option/{id}', [VotingController::class, 'addTotalOption']);
-	Route::delete('/votings/{id}', [VotingController::class, 'destroy']);
+	Route::get('/votings', [VotingController::class, 'index'])->middleware(['auth.api']);
+	Route::get('/votings/{id}', [VotingController::class, 'show'])->middleware(['auth.api']);
+	Route::post('/votings', [VotingController::class, 'store'])->middleware(['auth.api']);
+	Route::put('/votings/{id}', [VotingController::class, 'update'])->middleware(['auth.api']);
+	Route::put('/votings/option/{id}', [VotingController::class, 'addTotalOption'])->middleware(['auth.api']);
+	Route::delete('/votings/{id}', [VotingController::class, 'destroy'])->middleware(['auth.api']);
 
-	Route::get('/activities', [ActivityController::class, 'index']);
-	Route::get('/activities/{id}', [ActivityController::class, 'show']);
-	Route::post('/activities', [ActivityController::class, 'store']);
-	Route::put('/activities/{id}', [ActivityController::class, 'update']);
-	Route::delete('/activities/{id}', [ActivityController::class, 'destroy']);
+	Route::get('/activities', [ActivityController::class, 'index'])->middleware(['auth.api']);
+	Route::get('/activities/{id}', [ActivityController::class, 'show'])->middleware(['auth.api']);
+	Route::post('/activities', [ActivityController::class, 'store'])->middleware(['auth.api']);
+	Route::put('/activities/{id}', [ActivityController::class, 'update'])->middleware(['auth.api']);
+	Route::delete('/activities/{id}', [ActivityController::class, 'destroy'])->middleware(['auth.api']);
 
-	Route::get('/group_users', [GroupUserController::class, 'index']);
-	Route::get('/group_users/{id}', [GroupUserController::class, 'show']);
-	Route::post('/group_users', [GroupUserController::class, 'store']);
-	Route::put('/group_users/{id}', [GroupUserController::class, 'update']);
-	Route::delete('/group_users/{id}', [GroupUserController::class, 'destroy']);
+	Route::get('/group_users', [GroupUserController::class, 'index'])->middleware(['auth.api']);
+	Route::get('/group_users/{id}', [GroupUserController::class, 'show'])->middleware(['auth.api']);
+	Route::post('/group_users', [GroupUserController::class, 'store'])->middleware(['auth.api']);
+	Route::put('/group_users/{id}', [GroupUserController::class, 'update'])->middleware(['auth.api']);
+	Route::delete('/group_users/{id}', [GroupUserController::class, 'destroy'])->middleware(['auth.api']);
 
-	Route::get('/groups', [GroupController::class, 'index']);
-	Route::get('/groups/{id}', [GroupController::class, 'show']);
-	Route::post('/groups', [GroupController::class, 'store']);
-	Route::put('/groups/{id}', [GroupController::class, 'update']);
-	Route::delete('/groups/{id}', [GroupController::class, 'destroy']);
+	Route::get('/groups', [GroupController::class, 'index'])->middleware(['auth.api']);
+	Route::get('/groups/{id}', [GroupController::class, 'show'])->middleware(['auth.api']);
+	Route::post('/groups', [GroupController::class, 'store'])->middleware(['auth.api']);
+	Route::put('/groups/{id}', [GroupController::class, 'update'])->middleware(['auth.api']);
+	Route::delete('/groups/{id}', [GroupController::class, 'destroy'])->middleware(['auth.api']);
 
-    Route::get('/', [SiteController::class, 'index']);
+    Route::get('/', [SiteController::class, 'index'])->middleware(['auth.api']);
 
-    Route::post('/auth/login', [AuthController::class, 'login']); //->middleware(['signature']);
-    Route::post('/auth/logout', [AuthController::class, 'logout']); //->middleware(['signature']);
+    Route::post('/auth/login', [AuthController::class, 'login'])->middleware(['auth.api']); //->middleware(['signature']);
+    Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware(['auth.api']); //->middleware(['signature']);
     Route::get('/auth/profile', [AuthController::class, 'profile'])->middleware(['auth.api']);
 
-    Route::get('/users', [UserController::class, 'index']); //->middleware(['auth.api', 'role:user.view']);
-    Route::get('/users/{id}', [UserController::class, 'show']); //->middleware(['auth.api', 'role:user.view']);
-    Route::post('/users', [UserController::class, 'store']); //->middleware(['auth.api', 'role:user.create|roles.view']);
-    Route::put('/users/{id}', [UserController::class, 'update']); //->middleware(['auth.api', 'role:user.update||roles.view']);
-    Route::delete('/users/{id}', [UserController::class, 'destroy']); //->middleware(['auth.api', 'role:user.delete']);
-    Route::post('/user/update-password', [UserController::class, 'updatePassword']);
+    Route::get('/users', [UserController::class, 'index'])->middleware(['auth.api']); //->middleware(['auth.api', 'role:user.view'])->middleware(['auth.api']);
+    Route::get('/users/{id}', [UserController::class, 'show'])->middleware(['auth.api']); //->middleware(['auth.api', 'role:user.view'])->middleware(['auth.api']);
+    Route::post('/users', [UserController::class, 'store'])->middleware(['auth.api']); //->middleware(['auth.api', 'role:user.create|roles.view'])->middleware(['auth.api']);
+    Route::put('/users/{id}', [UserController::class, 'update'])->middleware(['auth.api']); //->middleware(['auth.api', 'role:user.update||roles.view'])->middleware(['auth.api']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->middleware(['auth.api']); //->middleware(['auth.api', 'role:user.delete'])->middleware(['auth.api']);
+    Route::post('/user/update-password', [UserController::class, 'updatePassword'])->middleware(['auth.api']);
 
-    Route::post('/forgot-password', [ForgotPasswordController::class, 'forgotPassword']);
+    Route::post('/forgot-password', [ForgotPasswordController::class, 'forgotPassword'])->middleware(['auth.api']);
 
-    Route::get('/roles', [RoleController::class, 'index']); //->middleware(['auth.api', 'role:roles.view']);
-    Route::get('/roles/{id}', [RoleController::class, 'show']); //->middleware(['auth.api', 'role:roles.view']);
-    Route::post('/roles', [RoleController::class, 'store']); //->middleware(['auth.api', 'role:roles.create']);
-    Route::put('/roles', [RoleController::class, 'update']); //->middleware(['auth.api', 'role:roles.update']);
-    Route::delete('/roles/{id}', [RoleController::class, 'destroy']); //->middleware(['auth.api', 'role:roles.delete']);
+    Route::get('/roles', [RoleController::class, 'index'])->middleware(['auth.api']); //->middleware(['auth.api', 'role:roles.view'])->middleware(['auth.api']);
+    Route::get('/roles/{id}', [RoleController::class, 'show'])->middleware(['auth.api']); //->middleware(['auth.api', 'role:roles.view'])->middleware(['auth.api']);
+    Route::post('/roles', [RoleController::class, 'store'])->middleware(['auth.api']); //->middleware(['auth.api', 'role:roles.create'])->middleware(['auth.api']);
+    Route::put('/roles', [RoleController::class, 'update'])->middleware(['auth.api']); //->middleware(['auth.api', 'role:roles.update'])->middleware(['auth.api']);
+    Route::delete('/roles/{id}', [RoleController::class, 'destroy'])->middleware(['auth.api']); //->middleware(['auth.api', 'role:roles.delete']);
 });
 
 Route::get('/', function () {
